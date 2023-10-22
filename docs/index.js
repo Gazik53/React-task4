5fc7935503bdb82d06e51df713231f9b9e011c2f
Gazik53 <122514225+Gazik53@users.noreply.github.com>
2023-10-22 15:42:38 +0300

    новый файл

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,6 @@
-function hello(name) {
-    return "<h1>My name is " + `${name}` + " and this is my homework.</h1>";
+function out(name) {
+    let p = document.getElementById('out');
+    p.innerHTML = "Любопытство — не порок, а источник знаний!";
+  return "<h1>My name is " + `${name}` + " and this is my homework.</h1>";
   }
   
- hello(Rustam)
